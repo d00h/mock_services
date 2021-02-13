b926aabc783bb05b9dfeb0576c5f93a9037fc238
Valerii Volkov <v.volkov@start.ru>
2021-02-13 19:23:37 +0300

    gunicorn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,9 @@ case "$1" in
  "flask-server")
      python mock_services/app.py 
     ;;
+ "gunicorn-server")
+    gunicorn --workers=4 --bind 0.0.0.0:5000 'mock_services.app:app'
+    ;;
  "tests")
      python -m pytest 
     ;;
