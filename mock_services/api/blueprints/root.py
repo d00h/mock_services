825a0a42d6cecc536b2879b5c5578f9b35415297
Valerii Volkov <v.volkov@start.ru>
2021-02-13 17:54:44 +0300

    index

--- mock_services/api/blueprints/root.py
+++ mock_services/api/blueprints/root.py
@@ -1,32 +1,16 @@
-from flask import Blueprint, render_template_string, url_for
+from flask import Blueprint, render_template_string 
 
 root = Blueprint('root', __name__)
 
 
 @root.route('/')
-def samples():
-    easysms_args = dict(
-        login='some login',
-        password='some_password',
-        ordinator='+7-499-123-56-12',
-        phone='+7-499-999-99-99',
-        text='hello world'
-    )
+def index():
     items = [
         dict(
             caption='ApiDocs', href='/apidocs'),
-        dict(
-            caption='EasySMS: default',
-            href=url_for('easysms.send_sms', profile='default', **easysms_args)),
-        dict(
-            caption='EasySMS: weak',
-            href=url_for('easysms.send_sms', profile='easysms_weak', **easysms_args)),
-        dict(
-            caption='EasySMS: sequence',
-            href=url_for('easysms.send_sms', profile='easysms_123', **easysms_args)),
-        dict(
-            caption='EasySMS: sms_limit',
-            href=url_for('easysms.send_sms', profile='easysms_limit', **easysms_args)),
+        # dict(
+        #     caption='EasySMS: weak',
+        #     href=url_for('easysms.send_sms', profile='easysms_weak', **easysms_args)),
     ]
     return render_template_string("""
     <html>
