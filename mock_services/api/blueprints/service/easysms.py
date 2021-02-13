698ae10b95b5f89708a3f1054065ea1c7ac5e06a
Valerii Volkov <v.volkov@start.ru>
2021-02-13 16:18:29 +0300

    blueprints/service

--- mock_services/api/blueprints/service/easysms.py
+++ mock_services/api/blueprints/service/easysms.py
@@ -6,13 +6,15 @@ from flask import Blueprint, jsonify, request, current_app
 easysms = Blueprint("easysms", __name__)
 
 
-@easysms.route('/', methods=['GET'], endpoint='send_sms')
+@easysms.route('/{profile}/', methods=['GET'], endpoint='send_sms')
 @args(profile=str)
 @query_string(login=opt(str), password=opt(str),
               ordinator=str, phone=str, text=str)
 def send_sms(profile, login, password, ordinator, phone, text):
-    return current_app.get_fake_response(
+    response = current_app.get_fake_response(
         profile=profile, endpoint=request.endpoint,
         login=login, password=password,
-        ordinator=ordinator, phone=phone, text=text) or \
-        jsonify(sms_id=uuid.uuid4(), text='Sended')
+        ordinator=ordinator, phone=phone, text=text)
+    if response is not None:
+        return response
+    return jsonify(sms_id=uuid.uuid4(), text='Sended')
--- mock_services/api/blueprints/service/mailgun.py
+++ mock_services/api/blueprints/service/mailgun.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 
-from covador import opt, split
+from covador import args, opt, split
 from covador.flask import form
-from flask import Blueprint, jsonify
+from flask import Blueprint, current_app, jsonify, request
 
 mailgun = Blueprint("mailgun", __name__)
 
 
-@mailgun.route('/messages', methods=['POST'])
+@mailgun.route('/{profile}/messages', methods=['POST'])
+@args(profile=str)
 @form(**{
     'from': str,
     'to': split(str),
@@ -17,6 +18,10 @@ mailgun = Blueprint("mailgun", __name__)
     'o:testmode': opt(str),
 })
 def messages(profile, **kwargs):
+    response = current_app.get_fake_response(
+        profile=profile, endpoint=request.endpoint, **kwargs)
+    if response is not None:
+        return response
     now = datetime.now()
     return jsonify(
         id='<{0:%Y%m%d%H%M%S}.{1}@mailgun.org>'.format(now, now.timestamp()),
@@ -24,8 +29,14 @@ def messages(profile, **kwargs):
     )
 
 
-@mailgun.route('/stats/total', methods=['GET'])
+@mailgun.route('/{profile}/stats/total', methods=['GET'])
+@args(profile=str)
 def stats(profile, **kwargs):
+    response = current_app.get_fake_response(
+        profile=profile, endpoint=request.endpoint, **kwargs)
+    if response is not None:
+        return response
+
     return jsonify(
         {
             "end": "Fri, 01 Apr 2012 00:00:00 UTC",
